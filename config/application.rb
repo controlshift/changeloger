bbb8b5d78084845157051dd3439c596b47bd131c
anero <anero79@gmail.com>
2025-06-04 18:01:39 -0300

    Fix deprecation message from dotenv

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
+
+require 'dotenv/load'
 
 module Changeloger
   class Application < Rails::Application
